f97ad72dfa67093a7e505aff12cd9575ff8c9952
Xantios Krugor <xantios@devbak.Sacredheart.lan>
2012-06-25 02:10:10 +0200

    Just an initial setup of the license and layout of the code Please bare in mint that this version is NOT functional in any way yet!
    - Xantios

---
+++ Classes/NameParser.class.php
@@ -0,0 +1,70 @@
+<?php
+/*
+    ============================================
+    Xantios Krugor  - Submarine
+    ===============================
+    
+    Usage of this file:
+        The most important function here would be parsing names ( Yeah really)
+        Just to convert filenames to an array, so we can use the bierdopje API
+        
+    Date of creation: 25 June 2012
+    Date of Update: Not yet used
+    
+    Copyright (c) 2012, Xantios Krugor
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+    3. All advertising materials mentioning features or use of this software
+   must display the following acknowledgement:
+   This product includes software developed by the Xantios Krugor.
+    4. Neither the name of Xantios Krugor nor the
+   names of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY XANTIOS KRUGOR ''AS IS'' AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL XANTIOS KRUGOR BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+/* Start of code, dont touch anything here if you are NOT a developer and dont want stuff to break! */
+
+
+//===========================================================
+// Class: NameParser
+// Usage: Default PHP Behaviour should be sufficient
+// Dependencies: none
+//===========================================================
+class NameParser
+{
+    
+    
+    //============================
+    // Function: __construct
+    // Usage: initializer
+    // return: void
+    //============================
+    public function __construct()
+    {
+        $aName = array();
+        
+    }
+}
+
+
+
+?>
